fc8f80f114013b19f7c41833f30dff919d21a9e3
Gurvinder Singh <avinashkaur842@gmail.com>
2025-09-14 02:16:33 +0530

    late init

---
+++ frontend/src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+
+let  hello = $state<any>(null);
+
+$effect(() => {
+  (async () => {
+    const response = await fetch("/hello");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch");
+    }
+
+    const data = await response.json();
+
+    hello = data;
+  })();
+});
+</script>
+
+<main>
+  <div style="padding: 24px">
+    <h1>Ripress + Svelte minimal demo</h1>
+    <div>
+      <h2>/hello</h2>
+      <pre>{hello ? JSON.stringify(hello, null, 2) : "Loading..."}</pre>
+    </div>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use ripress::{app::App, types::RouterFns};
+
+#[tokio::main]
+async fn main() {
+    let mut app = App::new();
+
+    app.static_files("/", "./dist").unwrap();
+
+    app.get("/hello", |_, res| async {
+        println!("Hello from Ripress!");
+        return res.text("Hello from Ripress!");
+    });
+
+    app.listen(3000, || {
+        println!("listening on http://localhost:3000");
+    })
+    .await
+}
